885dc6fd2624b4844855e41692d99816f9d3bfe7
ichuyko <ivandj@mail.ru>
2019-07-05 23:59:18 +0300

    ref server1 ts

--- app1/server1ts/src/app.ts
+++ app1/server1ts/src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import {Request, Response} from "express";
-import {User} from "../../shared/User";
 import {Login} from "../../shared/Login";
+import {User} from "../../shared/User";
 
 const app = express();
 
@@ -14,14 +14,13 @@ const getLogin = (): Login => {
 };
 
 app.use(express.static("../client/build"));
+
 app.get("/api/getUser", (req: Request, res: Response) => {
         res.send(getUser());
-
-    });
+});
 
 app.get("/api/getLogin", (req: Request, res: Response) => {
         res.send(getLogin());
-
-    });
+});
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
