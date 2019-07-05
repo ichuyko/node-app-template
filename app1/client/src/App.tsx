8f6846f17b22d860be48c906dde7652a89c6cd52
ichuyko <ivandj@mail.ru>
2019-07-06 00:26:27 +0300

    ref client1

--- app1/client/src/App.tsx
+++ app1/client/src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
+import React from "react";
 import {User} from "../../shared/User";
 
-interface Props {
+interface State {
     user: User;
 }
 
-interface State {
+interface Props {
     user: User;
 }
 
@@ -46,7 +46,7 @@ export default class App extends React.Component<Props, State> {
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
                     <p>
-                        User: {JSON.stringify(this.state.user.name ? this.state.user: this.props.user)}
+                        User: {JSON.stringify(this.state.user.name ? this.state.user : this.props.user)}
                     </p>
                     <div onClick={this.getUser}>getUser</div>
                     <div onClick={this.getUser2}>getUser2</div>
--- app1/client/src/index.tsx
+++ app1/client/src/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
 import {User} from "../../shared/User";
+import "./index.css";
 
 const user: User = {id: "0", name: "Default user"};
 
-ReactDOM.render(<App user={user}/>, document.getElementById('root'));
+ReactDOM.render(<App user={user}/>, document.getElementById("root"));
