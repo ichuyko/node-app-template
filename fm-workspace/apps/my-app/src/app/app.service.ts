45ef4411ff5a6235b45d60a0a68b2028f878820e
ichuyko <ivandj@mail.ru>
2019-10-11 21:01:57 +0300

    nx: add decorators to node lib class Post

--- fm-workspace/apps/my-app/src/app/app.service.ts
+++ fm-workspace/apps/my-app/src/app/app.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import {User} from "../../../../libs/my-lib/src/lib/types/User";
 import {isAdmin} from "../../../../libs/my-lib/src/lib/common/AdminUtils";
+import {Post} from "../../../../libs/my-node-lib/src/lib/dto/Post";
 
 @Injectable()
 export class AppService {
   getData(): { message: string } {
     const user: User = {id: 1, name: "Ivan"};
-    return ({ message: `Welcome to my-app! isOK: ${isAdmin(user).toString()}` });
+    const post: Post = {id: 1, title: "Ivan!!", rating: 1, text: "nest app!!"};
+    return ({ message: `Welcome to my-app! isOK: ${isAdmin(user).toString()} Post: ${JSON.stringify(post)}` });
   }
 }
